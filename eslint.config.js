617bb50c1eed7994847b4d22f761ef7385ab0ee4
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-01-31 17:04:33 -0300

    setup inicial configurado

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintconfigPrettier from 'eslint-config-prettier'
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended,],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +25,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'quotes': 'warn',
+      'semi': "warn",
     },
   },
+  eslintconfigPrettier
 )
